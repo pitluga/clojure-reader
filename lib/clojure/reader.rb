c7dfb2e3a6f976e084e917a368cb92320a6aa204
Tony Pitluga <tony.pitluga@gmail.com>
2011-11-13 18:08:16 -0600

    isolated the int <-> string manipulation

--- lib/clojure/reader.rb
+++ lib/clojure/reader.rb
@@ -1,22 +1,30 @@
 module Clojure
   class Reader
     MACROS = {
-      '"'[0] => :read_string,
-      '['[0] => :read_vector,
-      '('[0] => :read_list,
-      '{'[0] => :read_map
+      '"' => :read_string,
+      '[' => :read_vector,
+      '(' => :read_list,
+      '{' => :read_map
     }
 
+    def read_char(io)
+      io.readchar.chr
+    end
+
+    def push_char(io, char)
+      io.ungetc(char[0])
+    end
+
     def number?(b)
-      b.chr =~ /[0-9]/
+      b =~ /[0-9]/
     end
 
     def whitespace?(b)
-      b.chr =~ /\s/
+      b =~ /\s/
     end
 
     def read(io)
-      next_char = io.readchar
+      next_char = read_char(io)
       if number?(next_char)
         read_number(io, next_char)
       else
@@ -31,63 +39,63 @@ module Clojure
 
     def read_string(io)
       s = ""
-      next_char = io.readchar
-      until next_char == '"'[0]
+      next_char = read_char(io)
+      until next_char == '"'
         s << next_char
-        next_char = io.readchar
+        next_char = read_char(io)
       end
       s
     end
 
     def read_number(io, first_digit)
-      s = first_digit.chr
-      next_char = io.readchar
+      s = first_digit
+      next_char = read_char(io)
       until not number?(next_char)
         s << next_char
         break if io.eof?
-        next_char = io.readchar
+        next_char = read_char(io)
       end
       Integer(s)
     end
 
     def _read_delimited_list(io, delimiter)
       v = []
-      next_char = io.readchar
+      next_char = read_char(io)
       until next_char == delimiter
         reader_method = MACROS[next_char]
         unless reader_method.nil?
           v << send(reader_method, io)
         else
-          io.ungetc(next_char)
+          push_char(io, next_char)
           v << read(io)
         end
         break if io.eof?
-        next_char = io.readchar
+        next_char = read_char(io)
       end
       v
     end
 
     def read_list(io)
-      _read_delimited_list(io, ')'[0])
+      _read_delimited_list(io, ')')
     end
 
     def read_vector(io)
-      _read_delimited_list(io, ']'[0])
+      _read_delimited_list(io, ']')
     end
 
     def read_map(io)
-      list = _read_delimited_list(io, '}'[0])
+      list = _read_delimited_list(io, '}')
       alist = Enumerable::Enumerator.new(list, :each_cons, 2).map { |k,v| [k,v] }
       Hash[alist]
     end
 
     def read_token(io, char)
-      s = char.chr
-      next_char = io.readchar
+      s = char
+      next_char = read_char(io)
       until whitespace?(next_char)
         s << next_char
         break if io.eof?
-        next_char = io.readchar
+        next_char = read_char(io)
       end
       interpret_token(s)
     end
